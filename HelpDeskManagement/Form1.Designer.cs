b6d7886cdfc8963877fd69419dba2ce4f61189bd
handnn18411c@st.uel.edu.vn <handnn18411c@st.uel.edu.vn>
2020-04-19 21:40:17 +0700

    Add project files.

---
+++ HelpDeskManagement/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HelpDeskManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 42);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(19, 143);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(139, 143);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(85, 70);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(196, 20);
+            this.tbUsername.TabIndex = 5;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(85, 106);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(196, 20);
+            this.tbPassword.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::HelpDeskManagement.Properties.Resources._1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(744, 357);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+    }
+}
+
---
+++ HelpDeskManagement/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HelpDeskManagement.Libs;
+
+namespace HelpDeskManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(tbUsername.Text.Trim()!="" && tbPassword.Text.Trim()!="")
+            {
+                //MessageBox.Show("Đăng nhập thành công", "Thông báo");
+                //Cần kết nối với CSDL để kiểm tra
+                // Cách sử dụng các Parameters & Stored Procedure trong SQL 
+
+                // Bước 1. Gán giá trị cho các Parameters 
+                //Cách1: tạo các Parameters đầy đủ
+                SqlParameter[] sqlParams = {
+                new SqlParameter("@username",SqlDbType.NVarChar,30),
+                new SqlParameter("@password",SqlDbType.NVarChar,40)
+            };
+                sqlParams[0].Value = tbUsername.Text.Trim();
+                sqlParams[1].Value = Libs.Database.Data.HashBytesSHA1(tbPassword.Text.Trim()); //Mã hóa mật khẩu sang chuẩn HASH SH-A1 có chiều dài là 40 ký tự --> Mã hóa trực tuyến tại http://www.sha1-online.com/
+
+
+                // Cách2: tạo các Parameters ngắn gọn
+                SqlParameter[] sqlParams2 = {
+                new SqlParameter("@username",tbUsername.Text),
+                new SqlParameter("@password", Libs.Database.Data.HashBytesSHA1(tbPassword.Text.Trim()))
+            };
+
+                // Bước 3: Tạo ra một DataTable rỗng để chứa dữ liệu khi kết quả trả về
+                DataTable table = new DataTable();
+
+                //Bước 4: thực hiện kết nối 1 cấu SP cơ bản
+                try
+                {
+                   table= Libs.Database.Data.ExcuteToDataTable("USERNAME_SELECTCHECKLOGIN", CommandType.StoredProcedure,
+                        sqlParams);
+                    if(table.Rows.Count>0)
+                    {
+                        MessageBox.Show("Đăng nhập thành công", "Thông báo");
+                        String Username = table.Rows[0][0].ToString();
+                        String Department = table.Rows[0][3].ToString();
+                        MessageBox.Show("Chào bạn " + Username + " !\n bạn thuộc phòng "+Department, "Thông báo");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Đăng nhập thất bại", "Thông báo");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Kết nối bị lỗi", "THÔNG BÁO!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập liệu", "THÔNG BÁO!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
